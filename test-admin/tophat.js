96a4ec8bede63f28accaf386b99a2d915eb017b6
Melanie Wang <melanie.wang@shopify.com>
2020-12-04 16:17:39 -0500

    More clean up

--- test-admin/tophat.js
+++ test-admin/tophat.js
@@ -25,21 +25,21 @@ const headless = (process.env.HEADLESS || '1') === '1';
   const page = await browserContext.newPage();
   setupSpinner.succeed();
 
-  // await test('No accessibility issues on landing page', async () => {
-  //   await page.goto('https://shop1.myshopify.io/admin/');
-  //   await screenshot(page, 'landing-page');
-  //   // Inject and run axe-core
-  //   const handle = await page.evaluateHandle(`
-  //     // Inject axe source code
-  //     ${axeCore.source}
-  //     // Run axe
-  //     axe.run()
-  //   `);
-  //   results = await handle.jsonValue();
-  //   expect(results.violations.length).toBe(0);
-  //   // Destroy the handle & return axe results.
-  //   await handle.dispose();
-  // });
+  await test('No accessibility issues on landing page', async () => {
+    await page.goto('https://shop1.myshopify.io/admin/');
+    await screenshot(page, 'landing-page');
+    // Inject and run axe-core
+    const handle = await page.evaluateHandle(`
+      // Inject axe source code
+      ${axeCore.source}
+      // Run axe
+      axe.run()
+    `);
+    results = await handle.jsonValue();
+    expect(results.violations.length).toBe(0);
+    // Destroy the handle & return axe results.
+    await handle.dispose();
+  });
 
   await test('No accessibility violations on products page', async()=> {
     await page.goto('https://shop1.myshopify.io/admin/products');
